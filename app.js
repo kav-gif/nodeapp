50d297076dbc9b750b4c0a22da528899e3ed795d
kavinda <kavindadilhara129@gmail.com>
2022-03-08 10:19:00 +0530

    commit

--- app.js
+++ app.js
@@ -8,8 +8,8 @@ const port = process.env.PORT || 3000;
 app.engine('ejs',ejsMate)
 app.set('view engine','ejs');
 
-app.use(express.static(path.join(__dirname,'/public')))
-app.set('views',path.join(__dirname,'/views'))
+app.use(express.static(path.join(__dirname,'public')))
+app.set('views',path.join(__dirname,'views'))
 
 app.use('',homeRouter)
 
