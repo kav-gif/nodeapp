5b1ded8c99852b67fb85b0ea0c37aea16ed8d593
kavinda <kavindadilhara129@gmail.com>
2022-03-08 09:45:38 +0530

    changes to package.json and port

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const path = require('path');
 const ejsMate = require('ejs-mate');
 const homeRouter = require('./routers/home')
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.engine('ejs',ejsMate)
 app.set('view engine','ejs');
