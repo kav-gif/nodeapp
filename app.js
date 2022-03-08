ac238d56c58725f14a0fb117f5851a7d93b66838
kavinda <kavindadilhara129@gmail.com>
2022-03-08 22:39:33 +0530

    commit

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.engine('ejs',ejsMate)
 app.set('view engine','ejs');
 
 app.use(express.static(path.join(__dirname,'/public')))
-app.set('views',path.join(__dirname,'/views'))
+app.set('views',path.join(__dirname,'views'))
 
 app.use('/',homeRouter)
 
